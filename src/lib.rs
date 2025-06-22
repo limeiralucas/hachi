ad0f3ac1bd6b49591fd4b40f6c11ccfed86407f5
Lucas Limeira <lucasalveslm@gmail.com>
2025-06-22 11:38:45 -0300

    refactor: remove unused Register enum from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{io::{Error, ErrorKind, Read}, ops::Add};
+use std::{io::{Error, ErrorKind, Read}};
 use log::info;
 use rand::Rng;
 
@@ -20,25 +20,6 @@ const FONTSET: [u8; 80] = [
 	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
 	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 ];
-
-enum Register {
-    V0 = 0x0,
-    V1 = 0x1,
-    V2 = 0x2,
-    V3 = 0x3,
-    V4 = 0x4,
-    V5 = 0x5,
-    V6 = 0x6,
-    V7 = 0x7,
-    V8 = 0x8,
-    V9 = 0x9,
-    VA = 0xA,
-    VB = 0xB,
-    VC = 0xC,
-    VD = 0xD,
-    VE = 0xE,
-    VF = 0xF,
-}
 
 pub struct Chip8 {
     pub registers: [u8; 16],
